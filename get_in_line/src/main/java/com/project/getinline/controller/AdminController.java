457b4bb0e77c80f2c6e3875a68d41c000f75d64d
dearjh36 <dearjh36@naver.com>
2024-01-07 17:16:52 +0900

    Add #1 - add setting
    타임리프를 수월한 뷰 개발을 위해 추가
    5가지 뷰 컨트롤러 작성과 대응하는 html 파일 작성

---
+++ get_in_line/src/main/java/com/project/getinline/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.project.getinline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+    @GetMapping("/places")
+    public String adminPlaces(){
+        return "admin/places";
+    }
+
+    @GetMapping("/places/{placeId}")
+    public String adminPlaceDetail(@PathVariable Integer placeId){
+        return "admin/place-detail";
+    }
+
+    @GetMapping("/events")
+    public String adminEvent(){
+        return "admin/events";
+    }
+
+    @GetMapping("/events/{eventId}")
+    public String adminEventDetail(@PathVariable Integer eventId){
+        return "admin/event-detail";
+    }
+}
