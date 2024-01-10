71eb8963dc443d229c07f8d409e4b2590afca563
dearjh36 <dearjh36@naver.com>
2024-01-10 23:26:28 +0900

    Add #2 place view 설정
    admin controller로 place view 설정

--- get_in_line/src/main/java/com/project/getinline/controller/AdminController.java
+++ get_in_line/src/main/java/com/project/getinline/controller/AdminController.java
@@ -1,17 +1,31 @@
 package com.project.getinline.controller;
 
+import com.project.getinline.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
+
+    // @RequestParam 은 생략 가능, 생략하고 안하고 동작차이 있음
+    // 생략할시 @RequestParam의 required=false 로 설정
+    // 해당 메소드에서는 검색어가 들어가도 되고 안들어가도 되서 생략
+    // @RequestParam PlaceType placeType
     @GetMapping("/places")
-    public String adminPlaces(){
-        return "admin/places";
+    public ModelAndView adminPlaces(PlaceType placeType, String placeName, String address){
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/places/{placeId}")
