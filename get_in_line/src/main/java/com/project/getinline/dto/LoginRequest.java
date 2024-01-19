df35c3f3bb1b36a32cde47f521a95a69fd407e65
dearjh <dearjh36@naver.com>
2024-01-19 22:58:36 +0900

    Add AdminRequest, LoginRequest
    절대상수 Record 생성

---
+++ get_in_line/src/main/java/com/project/getinline/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.project.getinline.dto;
+
+public record LoginRequest(
+        String email,
+        String password
+) {
+    public static LoginRequest of(
+            String email,
+            String password
+    ) {
+        return new LoginRequest(email, password);
+    }
+}
+
