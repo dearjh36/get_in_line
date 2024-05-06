7763c85926b830666ac89db715af271c4e3a0c7d
dearjh36 <dearjh36@naver.com>
2024-05-07 00:16:38 +0900

    #12 Add
    AdminRepository 생성만 해둠

---
+++ get_in_line/src/main/java/com/project/getinline/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.project.getinline.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
+
