82c99e50844e476b0c33677f470f284f197f98e6
dearjh36 <dearjh36@naver.com>
2024-05-16 17:25:53 +0900

    #14 Remove
    Spring Data REST 로 API 를 만들어서 필요가 없어진 ApiController들 deprecated

--- get_in_line/src/main/java/com/project/getinline/controller/api/APIAuthController.java
+++ get_in_line/src/main/java/com/project/getinline/controller/api/APIAuthController.java
@@ -5,8 +5,15 @@ import com.project.getinline.dto.AdminRequest;
 import com.project.getinline.dto.LoginRequest;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Spring Data REST 로 API를 만들어서 당장 필요가 없어진 컨트롤러
+ * 우선 deprecated 하고, 향후 사용 방안을 고민해 본다.
+ * */
+
+/*
 @RequestMapping("/api")
-@RestController
+@RestController*/
+@Deprecated
 public class APIAuthController {
     @PostMapping("/sign-up")
     public APIDataResponse<String> signUp(@RequestBody AdminRequest adminRequest){
