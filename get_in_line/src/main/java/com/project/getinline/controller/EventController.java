3ffe94a637d1c09df10ffd03534459e59a6fa43b
dearjh36 <dearjh36@naver.com>
2024-05-18 15:47:20 +0900

    #14 Refactor

--- get_in_line/src/main/java/com/project/getinline/controller/EventController.java
+++ get_in_line/src/main/java/com/project/getinline/controller/EventController.java
@@ -1,26 +1,36 @@
 package com.project.getinline.controller;
 
-import com.project.getinline.constant.EventStatus;
+import com.querydsl.core.types.Predicate;
+import com.project.getinline.constant.ErrorCode;
+import com.project.getinline.domain.Event;
 import com.project.getinline.dto.EventResponse;
+import com.project.getinline.exception.GeneralException;
+import com.project.getinline.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RequiredArgsConstructor
 @RequestMapping("/events")
 @Controller
 public class EventController {
+
+    private final EventService eventService;
+
     @GetMapping
-    public ModelAndView events() {
+    public ModelAndView events(@QuerydslPredicate(root = Event.class) Predicate predicate) {
 
         Map<String, Object> map = new HashMap<>();
 
+        /*
         // TODO: 임시 데이터. 추후 삭제 예정
         map.put("events", List.of(EventResponse.of(
                     1L,
@@ -43,7 +53,14 @@ public class EventController {
                     24,
                     "마스크 꼭 착용하세요"
                 )
-        ));
+        ));*/
+
+        List<EventResponse> events = eventService.getEvents(predicate)
+                .stream()
+                .map(EventResponse::from)
+                .toList();
+
+        map.put("events", events);
 
         return new ModelAndView("event/index", map);
     }
@@ -52,6 +69,7 @@ public class EventController {
     public ModelAndView eventDetail(@PathVariable Long eventId) {
         Map<String, Object> map = new HashMap<>();
 
+        /*
         // TODO: 임시 데이터. 추후 삭제 예정
         map.put("event", EventResponse.of(
                 eventId,
@@ -63,7 +81,13 @@ public class EventController {
                 0,
                 24,
                 "마스크 꼭 착용하세요"
-        ));
+        ));*/
+
+        EventResponse event = eventService.getEvent(eventId)
+                .map(EventResponse::from)
+                .orElseThrow(() -> new GeneralException(ErrorCode.NOT_FOUND));
+
+        map.put("event",event);
 
         return new ModelAndView("event/detail", map);
     }
--- get_in_line/src/main/java/com/project/getinline/repository/EventRepository.java
+++ get_in_line/src/main/java/com/project/getinline/repository/EventRepository.java
@@ -1,16 +1,10 @@
 package com.project.getinline.repository;
 
-import com.project.getinline.constant.EventStatus;
 import com.project.getinline.domain.Event;
-import com.project.getinline.dto.EventDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 // todo완료 : 인스턴스 생성 편의를 위해 임시로 default 사용, repository layer 구현이 완성되면 삭제
 public interface EventRepository extends    JpaRepository<Event, Long>,
