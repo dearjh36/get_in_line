73d989c562f961f62467e4e52d13c4a8dd56a6c8
dearjh36 <dearjh36@naver.com>
2024-05-18 15:47:20 +0900

    #14 Refactor

--- get_in_line/src/main/java/com/project/getinline/dto/PlaceDTO.java
+++ get_in_line/src/main/java/com/project/getinline/dto/PlaceDTO.java
@@ -2,10 +2,12 @@ package com.project.getinline.dto;
 
 
 import com.project.getinline.constant.PlaceType;
+import com.project.getinline.domain.Place;
 
 import java.time.LocalDateTime;
 
 public record PlaceDto(
+        Long id,
         PlaceType placeType,
         String placeName,
         String address,
@@ -16,6 +18,7 @@ public record PlaceDto(
         LocalDateTime modifiedAt
 ) {
     public static PlaceDto of(
+            Long id,
             PlaceType placeType,
             String placeName,
             String address,
@@ -25,6 +28,24 @@ public record PlaceDto(
             LocalDateTime createdAt,
             LocalDateTime modifiedAt
     ) {
-        return new PlaceDto(placeType, placeName, address, phoneNumber, capacity, memo, createdAt, modifiedAt);
+        return new PlaceDto(id,placeType, placeName, address, phoneNumber, capacity, memo, createdAt, modifiedAt);
+    }
+
+    public static PlaceDto of(Place place) {
+        return new PlaceDto(
+                place.getId(),
+                place.getPlaceType(),
+                place.getPlaceName(),
+                place.getAddress(),
+                place.getPhoneNumber(),
+                place.getCapacity(),
+                place.getMemo(),
+                place.getCreatedAt(),
+                place.getModifiedAt()
+        );
+    }
+
+    public Place toEntity(){
+        return Place.of(placeType, placeName, address, phoneNumber, capacity,memo);
     }
 }
