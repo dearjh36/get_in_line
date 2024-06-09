67d0e243397e4e35c739e4a715f76a4f347b6136
dearjh36 <dearjh36@naver.com>
2024-06-09 16:36:12 +0900

    #18 Refactor PlaceControllerTest
    PlaceControllerTest.java
    placeId를 매개로 검색하게 수정

--- get_in_line/src/main/java/com/project/getinline/dto/PlaceDto.java
+++ get_in_line/src/main/java/com/project/getinline/dto/PlaceDto.java
@@ -17,6 +17,7 @@ public record PlaceDto(
         LocalDateTime createdAt,
         LocalDateTime modifiedAt
 ) {
+
     public static PlaceDto of(
             Long id,
             PlaceType placeType,
@@ -50,25 +51,15 @@ public record PlaceDto(
     }
 
     public Place updateEntity(Place place) {
-        if (placeType != null) {
-            place.setPlaceType(placeType);
-        }
-        if (placeName != null) {
-            place.setPlaceName(placeName);
-        }
-        if (address != null) {
-            place.setAddress(address);
-        }
-        if (phoneNumber != null) {
-            place.setPhoneNumber(phoneNumber);
-        }
-        if (capacity != null) {
-            place.setCapacity(capacity);
-        }
-        if (memo != null) {
-            place.setMemo(memo);
-        }
+        if (placeType != null) { place.setPlaceType(placeType); }
+        if (placeName != null) { place.setPlaceName(placeName); }
+        if (address != null) { place.setAddress(address); }
+        if (phoneNumber != null) { place.setPhoneNumber(phoneNumber); }
+        if (capacity != null) { place.setCapacity(capacity); }
+        if (memo != null) { place.setMemo(memo); }
 
         return place;
     }
+
 }
+
