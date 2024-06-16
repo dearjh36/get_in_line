7dc2b700c0aad1c5a5a84d01ce6769697dbf4c74
dearjh36 <dearjh36@naver.com>
2024-06-17 01:07:43 +0900

    #18

--- get_in_line/src/test/java/com/project/getinline/service/PlaceServiceTest.java
+++ get_in_line/src/test/java/com/project/getinline/service/PlaceServiceTest.java
@@ -1,7 +1,9 @@
 package com.project.getinline.service;
 
+import com.project.getinline.constant.ErrorCode;
 import com.project.getinline.constant.PlaceType;
 import com.project.getinline.dto.PlaceDto;
+import com.project.getinline.exception.GeneralException;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import com.project.getinline.domain.Place;
@@ -49,7 +51,39 @@ class PlaceServiceTest {
 
     @DisplayName("장소를 검색하는데 에러가 발생한 경우, 줄서기 프로젝트 기본 에러로 전환하여 예외 던진다.")
     @Test
-    void gi
+    void givenDataRelatedException_whenSearchingPlaces_thenThrowsGeneralException(){
+        // Given
+        RuntimeException e = new RuntimeException("This is test.");
+        given(placeRepository.findAll(any(Predicate.class))).willThrow(e);
+
+        // When
+        Throwable thrown = catchThrowable(() -> sut.getPlaces(new BooleanBuilder()));
+
+        // Then
+        assertThat(thrown)
+                .isInstanceOf(GeneralException.class)
+                .hasMessageContaining(ErrorCode.DATA_ACCESS_ERROR.getMessage());
+
+        then(placeRepository).should().findAll(any(Predicate.class));
+    }
+
+    @DisplayName("장소 ID로 장소를 조회하면, 빈 정보를 출력하여 보여준다.")
+    @Test
+    void givenPlaceId_whenSearchingNonexistentPlace_thenReturnsEmptyOptional(){
+        // Given
+        long placeId = 2L;
+        given(placeRepository.findById(placeId)).willReturn(Optional.empty());
+
+        // When
+        Optional<PlaceDto> result = sut.getPlace(placeId);
+
+        // Then
+        assertThat(result).isEmpty();
+        then(placeRepository).should().findById(placeId);
+
+    }
+
+
 
     private Place createPlace(PlaceType placeType, String placeName) {
         return createPlace(1L, placeType, placeName);
