458ec804d3d343cf9dff0fdbd82979338d8d816b
dearjh36 <dearjh36@naver.com>
2024-06-23 21:38:29 +0900

    #18

--- get_in_line/src/test/java/com/project/getinline/service/PlaceServiceTest.java
+++ get_in_line/src/test/java/com/project/getinline/service/PlaceServiceTest.java
@@ -2,12 +2,12 @@ package com.project.getinline.service;
 
 import com.project.getinline.constant.ErrorCode;
 import com.project.getinline.constant.PlaceType;
+import com.project.getinline.domain.Place;
 import com.project.getinline.dto.PlaceDto;
 import com.project.getinline.exception.GeneralException;
+import com.project.getinline.repository.PlaceRepository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
-import com.project.getinline.domain.Place;
-import com.project.getinline.repository.PlaceRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,7 +22,8 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @DisplayName("비즈니스 로직 - 장소")
 @ExtendWith(MockitoExtension.class)
@@ -117,6 +118,56 @@ class PlaceServiceTest {
                 .hasMessageContaining(ErrorCode.DATA_ACCESS_ERROR.getMessage());
         then(placeRepository).should().findById(any());
     }
+
+    @DisplayName("장소 정보를 주면, 장소를 생성하고 결과를 true로 보여준다.")
+    @Test
+    void givenPlace_whenCreating_thenCreatesPlaceAndReturnsTrue(){
+        // Given
+        Place place = createPlace(PlaceType.SPORTS,"체육관");
+        given(placeRepository.save(any(Place.class))).willReturn(place);
+
+        // When
+        boolean result = sut.createPlace(PlaceDto.of(place));
+
+        // Then
+        assertThat(result).isTrue();
+        then(placeRepository).should().save(any(Place.class));
+
+    }
+
+    @DisplayName("장소 정보를 주지 않으면, 생성 중단하고 결과를 false로 보여준다")
+    @Test
+    void givenNothing_whenCreating_thenAbortCreatingAndReturnsFalse(){
+        // Given
+
+        // When
+        boolean result = sut.createPlace(null);
+
+        // Then
+        assertThat(result).isFalse();
+        then(placeRepository).shouldHaveNoInteractions();
+
+    }
+
+    @DisplayName("장소 생성 중 데이터 예외가 발생하면, 줄서기 프로젝트 기본 예외로 전환하여 예외를 던진다.")
+    @Test
+    void givenDataRelatedException_whenCreating_thenThrowsGeneralException(){
+        // Given
+        Place place = createPlace(PlaceType.SPORTS, "체육관");
+        RuntimeException e = new RuntimeException("This is test");
+        given(placeRepository.save(any())).willThrow(e);
+
+        // When
+        Throwable thrown = catchThrowable(() -> sut.createPlace(PlaceDto.of(place)));
+
+        // Then
+        assertThat(thrown)
+                .isInstanceOf(GeneralException.class)
+                .hasMessageContaining(ErrorCode.DATA_ACCESS_ERROR.getMessage());
+        then(placeRepository).should().save(any());
+
+    }
+
 
     private Place createPlace(long id, PlaceType placeType, String placeName ){
         Place place = Place.of(
